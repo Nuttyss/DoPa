299ebd820d2b0d3f8a9ff52e89fba9fe2b12109e
Nuttyss <nityamraj03@gmail.com>
2025-05-17 16:21:43 +0530

    added support for uptimerobot

--- dummy_webserver.py
+++ dummy_webserver.py
@@ -1,4 +1,5 @@
 # dummy_webserver.py
+import os
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
@@ -9,7 +10,8 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.wfile.write(b"OK - DoPing is running")
 
 def run_server():
-    server = HTTPServer(('0.0.0.0', 10000), SimpleHandler)
+    PORT = int(os.environ.get("PORT", 10000))  # Use Render's PORT if set
+    server = HTTPServer(('0.0.0.0', PORT), SimpleHandler)
     server.serve_forever()
 
 def start():
